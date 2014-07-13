0d0307047655d6b961f0cd2adb90bb55e607f0fb
Natallia Perets <n.perets@intetics.com>
2014-07-13 12:50:30 +0300

    second commit

---
+++ Calc/Expression.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Calc
+{
+    class Expression
+    {
+        double arg1;
+        double arg2;
+        char operation;
+        double result;
+
+        public Expression(double arg1, double arg2, char operation)
+        {
+            this.arg1 = arg1;
+            this.arg2 = arg2;
+
+        }
+    }
+}
