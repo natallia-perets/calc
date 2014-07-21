531da46a01a53d554c457f1b0dfffe78dc4be93c
Natallia Perets <n.perets@intetics.com>
2014-07-21 15:06:06 +0300

    fix build

--- Calc/Program.cs
+++ Calc/Program.cs
@@ -1,21 +1,8 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Linq;
-<<<<<<< HEAD
-using System.Reflection;
-using System.Text;
-using System.Threading.Tasks;
 using System.Text.RegularExpressions;
 using Calculator;
 using Calculator.Operators;
-=======
-using System.Text;
-using System.Threading.Tasks;
-using Calc.Expressions;
-using Calc.Configurations;
-using System.Text.RegularExpressions;
->>>>>>> 9fb3bcd940d27c4460e6405425b0c7a329d81b13
-
 namespace Calc
 {
     class Program
@@ -24,7 +11,6 @@ namespace Calc
         {
             double arg1, arg2;
             char operation;
-<<<<<<< HEAD
             int accuracy = Config.GetAccuracy();
             IStorage storage = new TextFileStorage();
             List<IOperator> listop = new List<IOperator>();
@@ -34,13 +20,6 @@ namespace Calc
             string pattern = @"^(\s+)?(\-)?\d+(,\d+)?(\s+)?(\+|-|\*|/)(\s+)?(\-)?\d+(,\d+)?(\s+)?$";
             Console.WriteLine("Hello. This is CALCULATOR. It takes 2 operands and performs +-/* operations. You can adjust operation accuracy with the help of app.config file. For exit use Esc key");
             
-=======
-            int accuracy = Convert.ToInt32(Calc.Configurations.Config.GetConfigNumber());
-            ConsoleKeyInfo cki;
-            Calculator calc = new Calculator(accuracy);
-            string pattern = @"^(\s+)?(\-)?\d+(,\d+)?(\s+)?(\+|-|\*|/)(\s+)?(\-)?\d+(,\d+)?(\s+)?$";
-            Console.WriteLine("Hello. This is CALCULATOR. It takes 2 operands and performs +-/* operations. You can adjust operation accuracy with the help of app.config file. For exit use Esc key");
->>>>>>> 9fb3bcd940d27c4460e6405425b0c7a329d81b13
             while (true)
             {
                 Console.WriteLine("\nPress 'w' to write results to a file");
@@ -59,18 +38,9 @@ namespace Calc
                     try
                     {
                         arg1 = Convert.ToDouble(words[0]);
-<<<<<<< HEAD
                         operation = Convert.ToChar(words[1]);
                         arg2 = Convert.ToDouble(words[2]);
                         calc.AddExpression(arg1, arg2, operation);
-=======
-                        System.Console.WriteLine(words[0]);
-                        operation = Convert.ToChar(words[1]);
-                        arg2 = Convert.ToDouble(words[2]);
-                        Expression expr = new Expression(arg1, arg2, operation);
-                        Console.WriteLine(expr.ToString());
-                        calc.AddToHistoryList(expr);
->>>>>>> 9fb3bcd940d27c4460e6405425b0c7a329d81b13
                     }
                     catch (FormatException e)
                     {
@@ -85,7 +55,6 @@ namespace Calc
                 {
                     if (input.Equals("w"))
                     {
-<<<<<<< HEAD
                         storage.Save(calc.GetHistory());
                         continue;
                     }
@@ -95,25 +64,10 @@ namespace Calc
                         calc.ShowHistoryOnConsole();
                         continue;
                     }
-
-=======
-                        calc.WriteToFile(@"C:\Users\Natallia\Documents\Visual Studio 2013\Projects\Calc\Calc\results.txt");
-                        continue;
-                    }
-                    if (input.Equals("s"))
-                    {
-                        calc.ShowHistory();
-                        continue;
-                    }
->>>>>>> 9fb3bcd940d27c4460e6405425b0c7a329d81b13
                     if (input.Equals("q"))
                     {
                         Environment.Exit(1);
                     }
-<<<<<<< HEAD
-
-=======
->>>>>>> 9fb3bcd940d27c4460e6405425b0c7a329d81b13
                     Console.WriteLine("Wrong input");
                 }
             } 
